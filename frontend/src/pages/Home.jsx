60be096a337a643aba5908168e660880a0671d02
sanket Yadav <yadavsanket663@gmail.com>
2026-02-20 16:00:35 +0530

    Implement footer, best seller section, and add OurPolicy and NewsLetterBox components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Hero from '../components/section/Home/Hero'
 import LatestCollection from '../components/section/Home/LatestCollection'
 import BestSeller from '../components/section/Home/BestSeller'
+import OurPolicy from '../components/section/Home/OurPolicy'
+import NewsLetterBox from '../components/section/Home/NewsLetterBox'
 
 function Home() {
   return (
     <div>
       <Hero/>
-      {/* <LatestCollection/> */}
+      <LatestCollection/>
       <BestSeller/>
+      <OurPolicy/>
+      <NewsLetterBox/>
     </div>
   )
 }
