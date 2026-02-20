0d20913ffb01a9b917de123e3e8099db0ab3cf01
sanket Yadav <yadavsanket663@gmail.com>
2026-02-20 16:00:35 +0530

    Implement footer, best seller section, and add OurPolicy and NewsLetterBox components

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm ">
+        <div className="py-8">
+          <img className="w-32 mb-5" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-500">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
+            autem ad sapiente aspernatur ab inventore, minus commodi, possimus
+            impedit ducimus, cum officiis tempore excepturi quia accusamus
+            officia porro nulla nobis culpa aliquam nam aperiam nemo fugiat
+            totam. Voluptatum, illo quis.
+          </p>
+        </div>
+
+        <div>
+          <h1 className="text-xl font-semibold mb-5">COMPANY</h1>
+          <ul className="flex flex-col gap-1 text-gray-500">
+            <Link>Home</Link>
+            <Link>About Us</Link>
+            <Link>Delivery</Link>
+            <Link>Privacy Policy</Link>
+          </ul>
+        </div>
+
+        <div>
+          <h1 className="text-xl font-semibold mb-5">GET IN TOUCH</h1>{" "}
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+12222222323</li>
+            <li>Contact@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="w-full text-center">
+        <hr className="text-gray-400" />
+        <p className="py-5 text-sm text-center">
+          Copyright 1908@ StyleStake.dev - All Right Reserved.
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/components/section/Home/BestSeller.jsx
+++ frontend/src/components/section/Home/BestSeller.jsx
@@ -1,18 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Title from "../CommonComponet/Title";
 import ProductItem from "../CommonComponet/ProductItem";
-import { products } from "../../../assets/assets";
+import { ShopContext } from "../../../context/ShopContext";
 
 function BestSeller() {
-  const [marketbestSeller, setMarketBestSeller] = useState([]);
+  const { products } = useContext(ShopContext);
 
+  const [marketBestSeller, setMarketBestSeller] = useState([]);
   useEffect(() => {
-    setMarketBestSeller(products);
+    const bestProducts = products.filter((item) => item.bestseller);
+    setMarketBestSeller(bestProducts.slice(0, 5));
   }, []);
+
   return (
-  <div>
-{console.log(marketbestSeller)}
-  </div>);
+    <div>
+      {/* {console.log(marketBestSeller)} */}
+      <div className="text-center py-8 text-3xl ">
+        <Title text1={"BEST"} text2={"SELLER"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+          libero aperiam error consequatur..
+        </p>
+      </div>
+      {/* {REmdering Products} */}
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5  lg:grid-col-5 gap-4 gap-y-6">
+        {marketBestSeller.map((item, index) => {
+          return (
+            <ProductItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              image={item.image}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default BestSeller;
