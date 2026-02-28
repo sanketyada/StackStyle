a8f5281569d550b9225ae668bd5cba080a781f01
sanket Yadav <yadavsanket663@gmail.com>
2026-02-28 21:42:07 +0530

    Implement cart functionality with item count and toast notifications; enhance RelatedProducts and Products components

--- frontend/src/components/section/CommonComponet/RelatedProducts.jsx
+++ frontend/src/components/section/CommonComponet/RelatedProducts.jsx
@@ -1,31 +1,46 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { ShopContext } from "../../../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
-function RelatedProducts({ category, subCategory }) {
+function RelatedProducts({ category, subCategory,id }) {
   const { products } = useContext(ShopContext);
-  // console.log(category, subCategory);
-  // console.log(products)
-  const [filterProduct, setFilterProduct] = useState([]);
+  // const [relatedProduct, setRelatedProduct] = useState([]);
 
-  useState(() => {
-    setFilterProduct(products);
-  }, []);
+  // //useEffect are used when we have to do some side Effect like (Fetching,Subscribing,Using timers,Touching DOM)
+  // useEffect(()=>{
+  // if(products.length > 0){
+  //   let productCopy = products.slice()
+  //   productCopy = productCopy.filter((item)=>item.category == category)
+  //   productCopy= productCopy.filter((item)=>item.subCategory == subCategory)
+  //   setRelatedProduct(productCopy)
+  // }
+  // },[products, category, subCategory])
 
-  const applyFilter = ()=>{
-    let copyProduct = filterProduct
-    copyProduct = copyProduct.filter((item)=>item.category == category )
-    setFilterProduct(copyProduct)
-  }
-
-useEffect(()=>{
-  applyFilter()
-},[category, subCategory])
+  ////UseMemo be Appropriate Here use when (filtering, sorting,arrays,expensive calculations,prevent unnecessary recalculations on re-render)
+  const realatedData = useMemo(() => {
+    return products.filter(
+      (item) => item.category == category && item.subCategory == subCategory,
+    );
+  }, [products, category, subCategory]);
 
   return (
-    <div>
-      {filterProduct.map((item) => (
-        <img src={item.image[0]} alt="" />
-      ))}
+    <div className="my-24">
+      <div className="text-center text-3xl py-2 ">
+        <Title text1={"RELATED"} text2={"PRODUCTS"} />
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {realatedData.slice(0, 6).filter((item)=> item._id != id ).map((item, idx) =>(
+          <ProductItem
+            key={idx}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+          // console.log(item._id ==id)
+        ))}
+      </div>
     </div>
   );
 }
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,13 +1,56 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 const ShopContext = createContext();
 export { ShopContext };
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setshowSearch] = useState(true);
+  const [cartItems, setCartItem] = useState({});
+
+  const addToCart = async (itemId, ItemSize) => {
+    if (!ItemSize) {
+      toast.error("Select Product Size!");
+      return;
+    }
+    const cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][ItemSize]) {
+        cartData[itemId][ItemSize] += 1;
+      } else {
+        cartData[itemId][ItemSize] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][ItemSize] = 1;
+    }
+    setCartItem(cartData);
+  };
+
+  const getCartCount = () => {
+    let totalCount = 0;
+
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+
+    return totalCount;
+  };
+
+  
+  // useEffect(() => {
+  //   console.log(cartItems);
+  // }, [cartItems]);
 
   let value = {
     products,
@@ -17,6 +60,9 @@ const ShopContextProvider = ({ children }) => {
     setSearch,
     showSearch,
     setshowSearch,
+    cartItems,
+    addToCart,
+    getCartCount
   };
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
