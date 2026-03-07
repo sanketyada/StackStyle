3e13dd415cefb3f8da8ae526558144fb08eac845
sanket Yadav <yadavsanket663@gmail.com>
2026-03-07 19:54:33 +0530

    controller and model; implement Cloudinary for image uploads, enhance product listing and retrieval, and fix export issue in product model

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,5 +1,6 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
 //function for add product
-
 const addProduct = async (req, res) => {
   try {
     const {
@@ -7,50 +8,118 @@ const addProduct = async (req, res) => {
       description,
       price,
       category,
-      subCategory,
+      subcategory,
       sizes,
-      date,
       bestseller,
     } = req.body;
+    console.log(req.body);
 
-    const image1 = req.file.image1[0];
-    const image2 = req.file.image2[0];
-    const image3 = req.file.image3[0];
-    const image4 = req.file.image4[0];
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
 
-    console.log(
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item != undefined,
+    );
+    let imagesURL = await Promise.all(
+      images.map(async (image) => {
+        let result = await cloudinary.uploader.upload(image.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
+
+    const productData = {
       name,
       description,
-      price,
       category,
-      subCategory,
-      sizes,
-      date,
-      bestseller,
-    );
-    console.log(image1,image2,image3,image4)
-    res.json()
-  } catch (error) {
+      price: Number(price),
+      subCategory: subcategory,
+      bestseller: Boolean(bestseller),
+      sizes: JSON.parse(sizes),
+      image: imagesURL,
+      date: Date.now(),
+    };
+
+    const newProduct = new productModel(productData);
+    await newProduct.save();
+
     return res.json({
-        success:false,
-        message:error.message
-    })
+      success: true,
+      message: "Product Added.",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 
 //function for list product
-const listProduct = async () => {
-  console.log("Sanket");
+const listProduct = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    if (products.length < 0) {
+      return res.json({
+        success: false,
+        products,
+      });
+    }
+    return res.json({
+      success: true,
+      message: "Data Fetched",
+      products,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 //remove products
-const removeProduct = async (req,res) => {
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
     res.json({
-        message:"We are Free"
-    })
+      succes: true,
+      message: "Product Removed",
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 //Single product Info
-const singleProduct = async () => {};
+const singleProduct = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const product = await productModel.findById({ _id: id });
+    if (!product) {
+      return res.json({
+        success: false,
+        message: "Product Unable to find.",
+      });
+    }
+    return res.json({
+      success: true,
+      message: "Product Founded",
+      product,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export { addProduct, listProduct, removeProduct, singleProduct };
