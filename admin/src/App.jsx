f95d3c52054a1e3684acb6ab074e01eb496cc1f8
sanket Yadav <yadavsanket663@gmail.com>
2026-03-08 12:11:27 +0530

    Working on Admin pannel

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <AppRoutes/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ admin/src/routes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Layout from '../components/layout/Layout'
+import Add from '../pages/Add'
+import List from '../pages/List'
+import Orders from '../pages/Orders'
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+         <Route path='/' element={<Add/>}/>
+         <Route path='/list' element={<List/>}/>
+         <Route path='/orders' element={<Orders/>}/>
+      </Route>
+    </Routes>
+  )
+}
+
+export default AppRoutes
